85e8d9ecc9c2f9bc9fcd4591e52acc1a7651bd40
IgnacioGR <ignagilroca@gmail.com>
2021-06-10 22:36:35 +0200

    feat: :sparkles: added key support

--- index.js
+++ index.js
@@ -1,48 +1,77 @@
-for (let i = 0; i < document.querySelectorAll(".drum").length; i++) {
-  document.querySelectorAll(".drum")[i].addEventListener("click", whenClick);
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < numberOfDrumButtons; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+    var buttonInnerHTML = this.innerHTML;
+
+    makeSound(buttonInnerHTML);
+
+    buttonAnimation(buttonInnerHTML);
+  });
 }
 
-function whenClick() {
-  let buttonInnerHTML = this.innerHTML;
+document.addEventListener("keypress", function (event) {
+  makeSound(event.key);
 
-  switch (buttonInnerHTML) {
+  buttonAnimation(event.key);
+});
+
+function makeSound(key) {
+  switch (key) {
     case "w":
+    case "W":
       let crash = new Audio("sounds/crash.mp3");
       crash.play();
       break;
 
     case "a":
+    case "A":
       let kick = new Audio("sounds/kick-bass.mp3");
       kick.play();
       break;
 
     case "s":
+    case "S":
       let snare = new Audio("sounds/snare.mp3");
       snare.play();
       break;
 
     case "d":
+    case "D":
       let tom1 = new Audio("sounds/tom-1.mp3");
       tom1.play();
       break;
 
     case "j":
+    case "J":
       let tom2 = new Audio("sounds/tom-2.mp3");
       tom2.play();
       break;
 
     case "k":
+    case "K":
       let tom3 = new Audio("sounds/tom-3.mp3");
       tom3.play();
       break;
 
     case "l":
+    case "L":
       let tom4 = new Audio("sounds/tom-4.mp3");
       tom4.play();
       break;
 
     default:
-      console.log(buttonInnerHTML);
+      console.log(key);
       break;
   }
+}
+
+function buttonAnimation(currentKey) {
+  var activeButton = document.querySelector("." + currentKey);
+
+  activeButton.classList.add("pressed");
+
+  setTimeout(function () {
+    activeButton.classList.remove("pressed");
+  }, 100);
 }
